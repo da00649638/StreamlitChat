ec953eabdd3d82c2312cc4b7102c69378604babe
da00649638 <64843055+da00649638@users.noreply.github.com>
2026-05-05 05:14:02 +0000

    updated code

--- chat_demo_git.py
+++ chat_demo_git.py
@@ -7,21 +7,17 @@ st.title("I am your AI Assistant")
 with st.sidebar:
     st.title("Provide your api key")
     OPENAI_API_KEY = st.text_input("Enter your OpenAI API key", type="password")
+    llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
 
-
-llm = ChatOpenAI(model="gpt-4o",api_key=OPENAI_API_KEY)
 
 if not OPENAI_API_KEY:
     st.info("Please enter your OpenAI API key to continue")
     st.stop()
 
-
 question = st.text_input("Enter your question")
 
-if question:    
+if question:
     response = llm.invoke([
         HumanMessage(content=question)
     ])
     st.write(response.content)
-
- 
