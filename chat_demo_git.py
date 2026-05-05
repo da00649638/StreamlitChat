ebf1a22c58d6e4e11b03cd21b8569fed996e7fb9
X550870 <Ashok.Dubbaka@nissan-usa.com>
2026-05-05 09:29:12 +0530

    chat demo files

---
+++ chat_demo_git.py
@@ -0,0 +1,29 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+st.title("I am your AI Assistant")
+
+with st.sidebar:
+    st.title("Provide your api key")
+    OPENAI_API_KEY = st.text_input("Enter your OpenAI API key", type="password")
+
+
+llm = ChatOpenAI(model="gpt-4o",api_key=OPENAI_API_KEY)
+
+if not OPENAI_API_KEY:
+    st.info("Please enter your OpenAI API key to continue")
+    st.stop()
+
+
+question = st.text_input("Enter your question")
+
+try:
+    response = llm.invoke([
+        HumanMessage(content=question)
+    ])
+    st.write(response.content)
+
+except Exception as e:
+    st.write("ERROR TYPE:", type(e).__name__)
+    # st.write("ERROR:", e)
